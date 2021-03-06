930fcf97573d61b72ad89a44737099d71a3a6b71
MugdhaB <mugdha_bpt@yahoo.co.in>
2021-03-06 20:42:26 +0530

    Provided hint for nasa dummy key

--- list_consumer_producer/config.py
+++ list_consumer_producer/config.py
@@ -3,6 +3,8 @@ port=6379
 db=2
 queue_name = 'products'
 end_marker = "Done"
-nasa_demo_key = ""
+# You can get it from here https://api.nasa.gov/
+# Using 'Generate API Key' 
+nasa_demo_key = "Add your demo key here"
 folder_name = "images"
 zip_name = "images.zip"
